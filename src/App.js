b660fe4a579b40f39f54d8557e471187d82ff997
danmendozam <dmendozam@jumex.com.mx>
2020-08-21 10:31:36 -0500

    git

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Este si jalo perro.
+          Este si jalo perro con cambios y todo.
         </p>
         <a
           className="App-link"
