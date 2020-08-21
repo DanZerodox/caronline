4ab0799d5a2dd3e0c70568b9b546f71ec47c5908
danmendozam <dmendozam@jumex.com.mx>
2020-08-21 12:12:18 -0500

    primera actualizacion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import { Productos } from './componentes/Productos';
+import { DetalleProducto } from "./DetalleProducto";
 
 class App extends React.Component{
   render(){
     return(
       <BrowserRouter>
-      <Productos></Productos>
+        <Route path="/detalleproducto/:id" exact={true} component={DetalleProducto}></Route>
+        <Route path="/" exact={true} component={Productos}></Route>
       </BrowserRouter>  
      )
   }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {HeadTop} from './componentes/HeadTop';
+import {HeadBanner} from './componentes/HeadBanner';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <>
+    <HeadTop/>
+    <HeadBanner></HeadBanner>
     <App />
+    </>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
