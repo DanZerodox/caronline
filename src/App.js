8bc060b69f6203f7d823699b99d1c0c03c35d13c
danmendozam <dmendozam@jumex.com.mx>
2020-08-21 12:46:33 -0500

    Actualizacion final logica

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import {render} from 'react-dom';
+import {Productos} from './componentes/Productos';
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import { Productos } from './componentes/Productos';
 import { DetalleProducto } from "./DetalleProducto";
+import { FormularioCompra } from './FormularioCompra';
+import { FormularioRegistro } from './FormularioRegistro';
+import { ContenidoCarrito } from './ContenidoCarrito';
+import { Direcciones } from './Direcciones';
+import { ResumenCompra } from './ResumenCompra';
+import { Historico } from './Historico';
+import { CarritoResponsivo } from './componentes/CarritoResponsivo';
 
 class App extends React.Component{
   render(){
-    return(
-      <BrowserRouter>
-        <Route path="/detalleproducto/:id" exact={true} component={DetalleProducto}></Route>
-        <Route path="/" exact={true} component={Productos}></Route>
-      </BrowserRouter>  
-     )
-  }
-
+    return (
+      <>          
+       <BrowserRouter>
+       <Route path="/detalleproducto/:id" exact={true} component={DetalleProducto}></Route>
+       <Route path="/formulariocompra" exact={true} component={FormularioCompra}></Route>
+       <Route path="/registrocuenta" exact={true} component={FormularioRegistro}></Route>
+       <Route path="/contenidocarrito" component={ContenidoCarrito}></Route>
+       <Route path="/direcciones" exact={true} component={Direcciones}></Route>
+       <Route path="/resumen" exact={true} component={ResumenCompra}></Route>
+       <Route path="/historico" exact={true} component={Historico}></Route>
+       <Route path="/carritoresponsive" exact={true} component={CarritoResponsivo}></Route>
+       <Route path="/" exact={true} component={Productos}></Route>
+       </BrowserRouter>  
+            
+       </>
+    );
+  };
 }
 export default App;
